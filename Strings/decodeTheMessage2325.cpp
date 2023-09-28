8c83a19434f11c35c0495ff4597fd5acb52d345a
Sapnendra Jaiswal <sapnendrajaiswal2388@gmail.com>
2023-09-28 10:27:46 +0530

    add new leetcode que

---
+++ Strings/decodeTheMessage2325.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string decodeMessage(string key, string message) {
+        // create mapping
+
+    char start = 'a';
+        char mapping[1000] = {0};
+        for(auto ch: key) {
+            if(ch != ' ' && mapping[ch] == 0) {
+                mapping[ch] = start;
+                start++;
+            }
+        }
+
+        // ans string
+        string ans;
+        for(auto ch:message) {
+            if(ch == ' ') {
+                ans.push_back(' ');
+            } else {
+                ans.push_back(mapping[ch]);
+            }
+        }
+
+        return ans;
+    }
+};
