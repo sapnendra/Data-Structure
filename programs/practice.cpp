86771ccb6b8245f29f47e62e42f6049850e6493f
Sapnendra Jaiswal <sapnendrajaiswal2388@gmail.com>
2023-10-08 16:28:44 +0530

    add new files and modified

--- programs/practice.cpp
+++ programs/practice.cpp
@@ -4,28 +4,6 @@ using namespace std;
 
 
 int main() {
-    int a = 100;
-    int* ptr = &a;
-    int* cptr = ptr;
-
-    cout << "a: " << a << endl;
-    cout << "&a: " << &a << endl;
-    // cout << "*a: " << *a << endl;  // error
-    cout << "ptr: " << ptr << endl;
-    cout << "&ptr: " << &ptr << endl;
-    cout << "*ptr: " << *ptr << endl;
-
-    cout << "cptr: " << cptr << endl;
-    cout << "&cptr: " << &cptr << endl;
-    cout << "*cptr: " << *cptr << endl;
-
-    cout << "(*ptr)++ : " << (*ptr)++ << endl;
-    cout << "++(*ptr): " << ++(*ptr) << endl;
     
-    *ptr = *ptr/2;
-    cout << "divide: " << *ptr << endl;
-    *ptr = *ptr-2;
-    cout << "(*ptr-2): " << *ptr << endl;
-
     return 0;
 }
