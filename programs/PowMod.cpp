4ccf9e7022ad4470d08f054056a9ed00412dd03f
Sapnendra Jaiswal <sapnendrajaiswal2388@gmail.com>
2023-10-12 21:11:14 +0530

    add new files

--- programs/PowMod.cpp
+++ programs/PowMod.cpp
@@ -1,10 +1,33 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int main() {
-    int a;
-    int b;
+class Solution {
+public:
+    long long int PowMod(int x, int n, int m) {
+        long long int ans = 1;
+        while( n > 0) {
+            if( n&1 ) {
+                // n is odd 
+                ans = (ans*x)%m;
+            }
 
-    cin >> a >> b;
+            x = (x*x) % m;
+            n = n>>1;
+        }
+        return ans;
+    }
+};
+
+int main() {
+    int t;
+    cin >> t;
+    while(t--) {
+        long long int x, n, m;
+        cin >> x >> n >> m;
+
+        Solution ob;
+        long long int ans = ob.PowMod(x, n, m);
+        cout << ans << endl;
+    }
 return 0;
 }
