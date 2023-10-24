4cd1c892c5c6610a42ce7539f1d9640880dbbfe7
Sapnendra Jaiswal <sapnendrajaiswal2388@gmail.com>
2023-10-24 12:33:11 +0530

    add new and modified files

--- DnC-BackTracking/programs/permutationOfString.cpp
+++ DnC-BackTracking/programs/permutationOfString.cpp
@@ -1,13 +1,26 @@
 #include<iostream>
 using namespace std;
 
-void backTrack(string str, int index) {
-    
+void backTrack(string &str, int index) {
+    // base case
+    if(index >= str.length()) {
+        cout << str << " ";
+        return;
+    }
+
+    for(int j=index; j<str.length(); j++) {
+        swap(str[index], str[j]);
+        backTrack(str, index+1);
+        swap(str[index], str[j]);
+    }
 }
 
 int main() {
     string str = "abc";
     int index = 0;
+    cout << "Input string: " << str << endl;
+    cout << "Permutations: " << endl;
     backTrack(str, index);
+    cout << endl;
     return 0;
 }
