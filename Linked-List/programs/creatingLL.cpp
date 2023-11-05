87e7e1c4e22e337b8fabd56c9371c53ea17c8826
Sapnendra Jaiswal <sapnendrajaiswal2388@gmail.com>
2023-11-05 22:11:56 +0530

    git add .

---
+++ Linked-List/programs/creatingLL.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node{
+public:
+    int data;
+    Node* next;
+
+    Node() {
+        this->next=NULL;
+    }
+
+    Node(int data) {
+        this->data = data;
+        this->next = NULL;
+    }
+};
+
+void print(Node* head) {
+    Node* temp = head;
+    while(temp != NULL) {
+        cout << temp->data << "->";
+        temp = temp->next;
+    }
+}
+
+int main() {
+    // creating first node
+    // Node head(10); - static initialization
+    Node* first = new Node(10);  // dynamic initialization
+    Node* second = new Node(20);
+    Node* third = new Node(30);
+    Node* fourth = new Node(40);
+    Node* fifth = new Node(50);
+
+    Node* head = first;
+
+    first->next = second;
+    second->next = third;
+    third->next = fourth;
+    fourth->next = fifth;
+
+    cout << "Printng entire list: " << endl;
+    print(head);
+
+    return 0;
+}
