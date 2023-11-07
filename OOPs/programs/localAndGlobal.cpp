0c3370fa182b548a3b02fc86f3eba1619b7e80a9
Sapnendra Jaiswal <sapnendrajaiswal2388@gmail.com>
2023-11-07 13:27:23 +0530

    add some notes of 2.0

--- OOPs/programs/localAndGlobal.cpp
+++ OOPs/programs/localAndGlobal.cpp
@@ -1,9 +1,12 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+int num = 100;  // global variable
+
 int main() {
-    for(int i=0; i<6; i++) {
-        cout << "Hello" << endl;
-    }
+    num = 200;  // global x
+    int num = 20;
+    cout << num << endl; // local variable
+    cout << ::num << endl; // global variable accessing inside main function using scope resolution operator
     return 0;
 }
