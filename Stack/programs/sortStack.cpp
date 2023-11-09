c50709bcbcbb0815c4d3dfc7b3f432440311c260
Sapnendra Jaiswal <sapnendrajaiswal2388@gmail.com>
2023-11-10 00:10:50 +0530

    add new stack que

--- Stack/programs/sortStack.cpp
+++ Stack/programs/sortStack.cpp
@@ -4,10 +4,13 @@ using namespace std;
 
 void insertInSortedStack(stack<int> &st, int data) {
     // base case
+
+    // cout << "I am here" << endl;
     if(st.empty() || data > st.top()) {
         st.push(data);
         return;
     }
+    // cout << "I am here again" << endl;
 
     // one case solution
     int temp = st.top();
@@ -18,13 +21,32 @@ void insertInSortedStack(stack<int> &st, int data) {
     st.push(temp);  // backtrack
 }
 
+void sortStack(stack<int> &st) {
+    // base case
+    if(st.empty()) {
+        return;
+    }
+
+    // one case solution
+    int temp = st.top();
+    st.pop();
+
+    // recursion call
+    sortStack(st);
+    insertInSortedStack(st, temp);  // backtrack
+}
+
 int main() {
     stack<int> st;
-    st.push(10);
     st.push(20);
+    st.push(90);
     st.push(30);
+    st.push(65);
+    st.push(50);
+    st.push(10);
 
-    insertInSortedStack(st, 0);
+    sortStack(st);
+    
     while(!st.empty()) {
         cout << st.top() <<" ";
         st.pop(); 
