d88c32f0293a940dbefd2cdb358df48fa9c74b11
Sapnendra Jaiswal <sapnendrajaiswal2388@gmail.com>
2024-02-05 08:22:26 +0530

    add binary search tree section

--- BinarySearchTree/programs/implementBST.cpp
+++ BinarySearchTree/programs/implementBST.cpp
@@ -1,30 +1,159 @@
-#include<iostream>
+#include <iostream>
+#include <queue>
 using namespace std;
 
-class Node {
+class Node
+{
 public:
     int data;
-    Node* left;
-    Node* right;
+    Node *left;
+    Node *right;
 
-    Node(int data) {
+    Node(int data)
+    {
         this->data = data;
         this->left = NULL;
         this->right = NULL;
     }
 };
 
-Node* createBST() {
-    cout << "Enter Data : " << endl;
+Node* insertIntoBST(Node *root, int data)
+{
+    if (root == NULL)
+    {
+        root = new Node(data);
+        return root;
+    }
+
+    // its not the first node
+    if (data > root->data)
+    {
+        root->right = insertIntoBST(root->right, data);
+    }
+    else
+    {
+        root->left = insertIntoBST(root->left, data);
+    }
+
+    return root;
+}
+
+void createBST(Node* &root)
+{
+    cout << "Enter Data : ";
     int data;
     cin >> data;
 
-    while(data != -1) {
-         
+    while (data != -1)
+    {
+        root = insertIntoBST(root, data);
+        cout << "Enter data : ";
+        cin >> data;
     }
 }
 
-int main() {
+void levelOrderTraversal(Node *root)
+{
+    queue<Node*> q;
+    q.push(root);
+    q.push(NULL);
+
+    while (!q.empty())
+    {
+        Node* temp = q.front();
+        q.pop();
+
+        if (temp == NULL)
+        {
+            cout << endl;
+            if (!q.empty())
+            {
+                q.push(NULL);
+            }
+        }
+        else
+        {
+            cout << temp->data << " ";
+            if (temp->left != NULL)
+            {
+                q.push(temp->left);
+            }
+            if (temp->right != NULL)
+            {
+                q.push(temp->right);
+            }
+        }
+    }
+}
+
+// preorder
+void preorder(Node* root) {
+    if(root == NULL) {
+        return;
+    }
+
+    cout << root->data << " ";
+    preorder(root->left);
+    preorder(root->right);
+}
+
+// inorder
+void inorder(Node* root) {
+    if(root == NULL) {
+        return;
+    }
+
+    inorder(root->left);
+    cout << root->data << " ";
+    inorder(root->right);
+}
+
+// postorder
+void postorder(Node* root) {
+    if(root == NULL) {
+        return;
+    }
+
+    postorder(root->left);
+    postorder(root->right);
+    cout << root->data << " ";
+}
+
+// min value
+Node* findMinValueNode(Node* root) {
+    if(root == NULL) {
+        return NULL;
+    }
+    Node* temp = root;
+
+    while(temp->left != NULL) {
+        temp = temp->left;
+    }
+
+    return temp;
+}
+
+int main()
+{
+    Node *root = NULL;
+    createBST(root);
+    
+    cout << endl << "Level Order Traversal : "<< endl;
+    levelOrderTraversal(root);
+
+    Node* minNode = findMinValueNode(root);
+    if(minNode == NULL) {
+        cout << "Entire tree in NULL, No such value exist." << endl;
+    } else {
+        cout << "Min value node is : " << minNode->data << endl;
+    }
+
+    // cout << endl << "Pre-Order Traversal : "<< endl;
+    // preorder(root);
+    // cout << endl << "In-Order Traversal : "<< endl;
+    // inorder(root);
+    // cout << endl << "Post-Order Traversal : "<< endl;
+    // postorder(root);
 
     return 0;
 }
